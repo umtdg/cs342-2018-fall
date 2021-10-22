628aa22c3baddb7ace63f7aaed25ae9a65fd4b8f
Umut Dağ <dagumut604@gmail.com>
2021-10-22 11:29:40 +0300

    implement syn_phistogram

--- project1/syn_phistogram.c
+++ project1/syn_phistogram.c
@@ -1,100 +1,100 @@
 #include <sys/mman.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-#include <semaphore.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "helper.h"
 
-#define SEM_NAME "/histsem\0"
+#define SEM_NAME "/histsem"
 
-#define SHM_NAME "/histshm\0"
+#define SHM_NAME "/histshm"
 
 int
-main(void) {
-    if (create_sem(SEM_NAME) == -1) exit(EXIT_FAILURE);
-
-    if (create_shm(SHM_NAME, sizeof(int)) == -1) {
-        sem_unlink(SEM_NAME);
-        exit(EXIT_FAILURE);
+main(int argc, char **argv) {
+    if (argc < 6) {
+        return 0;
     }
 
-    int proc_count = 5;
-    for (int i = 0; i < proc_count; i++) {
-        pid_t pid = fork();
-        if (pid == -1) {
+    double min, max;
+    size_t bin_count, file_count;
+    size_t shm_size;
+
+    sscanf(argv[1], "%lf", &min);
+    sscanf(argv[2], "%lf", &max);
+    sscanf(argv[3], "%lu", &bin_count);
+    sscanf(argv[4], "%lu", &file_count);
+    shm_size = sizeof(int) * bin_count;
+
+    if ((size_t)argc < (6U + file_count)) {
+        return 0;
+    }
+
+    if (create_sem(SEM_NAME) == -1) exit(EXIT_FAILURE);
+
+    if (create_shm(SHM_NAME, shm_size) == -1)
+        exit(EXIT_FAILURE);
+
+    pid_t pid;
+    for (size_t i = 5; i < file_count + 5; i++) {
+        if ((pid = fork()) == -1) {
             perror("fork");
-            return 1;
+            exit(EXIT_FAILURE);
         }
 
         if (pid == 0) {
+            size_t lines = line_count(argv[i]);
+            size_t *hist = hist_from_file(argv[i], lines, min, max, bin_count);
+            if (hist == NULL) {
+                _exit(EXIT_FAILURE);
+            }
+
             sem_t *sem = open_wait_sem(SEM_NAME);
             if (sem == NULL) _exit(EXIT_FAILURE);
 
             int fd;
-            int *shmp = (int *)get_shm(SHM_NAME, sizeof(int), &fd);
+            size_t *shmp = (size_t *)get_shm(SHM_NAME, shm_size, &fd);
             if (shmp == NULL) _exit(EXIT_FAILURE);
 
-            *shmp += 5;
+            for (size_t i = 0; i < bin_count; i++) {
+                shmp[i] += hist[i];
+            }
 
-            if (cleanup_shm(shmp, SHM_NAME, sizeof(int), fd) == -1)
+            if (cleanup_shm(shmp, SHM_NAME, shm_size, fd) == -1)
                 _exit(EXIT_FAILURE);
 
             if (post_close_sem(sem, SEM_NAME) == -1)
                 _exit(EXIT_FAILURE);
-           
-            exit(EXIT_SUCCESS);
+
+            _exit(EXIT_SUCCESS);
         }
     }
 
     int status;
-    while (proc_count > 0) {
+    size_t pid_count = file_count;
+    while (pid_count > 0) {
         wait(&status);
+
         if (status != EXIT_SUCCESS) {
-           sem_unlink(SEM_NAME);
-           shm_unlink(SHM_NAME);
-           exit(EXIT_FAILURE);
+            sem_unlink(SEM_NAME);
+            shm_unlink(SHM_NAME);
+            exit(EXIT_FAILURE);
         }
-        --proc_count;
+
+        --pid_count;
     }
 
-    sem_t *histsem;
-    if ((histsem = open_wait_sem(SEM_NAME)) == NULL) {
-        shm_unlink(SHM_NAME);
-        exit(EXIT_FAILURE);
-    }
+    sem_unlink(SEM_NAME);
 
-    int shm_hist_fd;
-    int *shmp = (int *)get_shm(SHM_NAME, sizeof(int), &shm_hist_fd);
-    if (shmp == NULL) {
-        post_close_sem(histsem, SEM_NAME);
-        sem_unlink(SEM_NAME);
-        exit(EXIT_FAILURE);
-    }
+    int fd;
+    size_t *shmp = (size_t *)get_shm(SHM_NAME, shm_size, &fd);
+    if (shmp == NULL) exit(EXIT_FAILURE);
 
-    printf("Total: %d\n", *shmp);
+    save_hist_to_file(shmp, bin_count, argv[5U + file_count], 1);
 
-    if (cleanup_shm(shmp, SHM_NAME, sizeof(int), shm_hist_fd) == -1) {
-        post_close_sem(histsem, SEM_NAME);
-        sem_unlink(SEM_NAME);
-        exit(EXIT_FAILURE);
-    }
+    cleanup_shm(shmp, SHM_NAME, shm_size, fd);
 
-    post_close_sem(histsem, SEM_NAME);
+    shm_unlink(SHM_NAME);
 
-    if (sem_unlink(SEM_NAME) == -1) {
-        perror("sem_unlink");
-        return 1;
-    }
-
-    if (shm_unlink(SHM_NAME) == -1) {
-        perror("shm_unlink");
-        return 1;
-    }
-
-    return 0;
+    exit(EXIT_SUCCESS);
 }
 
